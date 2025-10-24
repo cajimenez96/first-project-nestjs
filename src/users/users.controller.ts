edb20ca0a328a1944c0183a46abb3616c5b01bbf
cajimenez96 <carlos.exejimenez@gmail.com>
2025-10-24 15:49:55 -0300

    feat: implement Users module with controller and service

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller()
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {
+    this.usersService = usersService;
+  }
+
+  @Get('/users')
+  getAllUsers() {
+    return this.usersService.getUsers();
+  }
+}
